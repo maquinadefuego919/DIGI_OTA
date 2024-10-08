614bc77cf655a9b2962d76ebb96c2b70659e3229
Abnerpfff1 <parra.abner@uabc.edu.mx>
2024-10-08 13:59:46 -0700

    Update DIGI_OTA.v

--- src/DIGI_OTA.v
+++ src/DIGI_OTA.v
@@ -18,25 +18,31 @@ module tt_um_DIGI_OTA (
     wire Out, Vip, Vin;
   // All output pins must be assigned. If not used, assign to 0.
     assign [0] uo_out  = Out;  // Example: ou_out is the sum of ui_in and uio_in
-    assign uo_out[7:1] = 7'b0000000
+    assign uo_out[7:1] = 7'b0000000:
     
     
     assign [0] ui_in  = Vip;
     assign [1] ui_in  = Vin;
 
-wire INn, INp, CMP, EN, not_EN, Op, On, Pr; //internals nets
+wire INn, INp, CMP, EN, not_EN, Op, On, Pr, INn_CMP, INp_CMP, INp_And, INn_And; //internals nets
 not IV1 (INn, Vip);
 not IV2 (INn, CMP);
 not IV3 (INp, Vin);
 not IV4 (INp, CMP);
-not IV5 (Op, INn);
-not IV6 (On, INp);
+
+    xor XOR1 (INn_And, INn, IN_CMP):
+    xor XOR2 (INp_And, INp, Inp_CMP);
+    
+    not IV5 (Op, INn_CMP);
+    not IV6 (On, INp_CMP);
+    
 not IV7 (not_EN, EN);
 xor XOR1 (EN, Op, On);
     bufif1 BT1 (Pr, EN, Op);
 notif1 IT1 (CMP, not_EN, Op);
-    buf Buf1 (Out, Pr);
-
+   
+  assign uio_out = 0;
+    assign uio_oe  = 0;
     
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
