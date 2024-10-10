57f9f4a29af5866e44849158999b31a9c5189b50
Abnerpfff1 <parra.abner@uabc.edu.mx>
2024-10-10 15:45:29 -0700

    Update tt_um_DIGI_OTA.v

--- src/tt_um_DIGI_OTA.v
+++ src/tt_um_DIGI_OTA.v
@@ -30,8 +30,9 @@ not IV2 (INn, CMP);
 not IV3 (INp, Vin);
 not IV4 (INp, CMP);
 
-xor XOR1 (INn_And, INn, INn_CMP):
-xor XOR2 (INp_And, INp, Inp_CMP);
+    
+nor NOR1 (INn_And, INn, INn_CMP):
+nor NOR2 (INp_And, INp, Inp_CMP);
     
 not IV5 (Op, INn_CMP);
 not IV6 (On, INp_CMP);
